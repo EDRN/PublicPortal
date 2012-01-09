109687b99a97be42f5625d3a6e90768c2cdb0b73
Sean Kelly <sean.c.kelly@me.com>
2012-01-09 21:39:36 +0000

    Update to support both the ZODB snapshots and the ZODB blob stroage

--- support/getsnapshot.py
+++ support/getsnapshot.py
@@ -5,56 +5,72 @@
 
 '''Get the latest database snapshot of the Early Detection Research Network's public portal.'''
 
-import logging, sys, getpass, os, os.path, urllib2
+import logging, sys, getpass, os, os.path, urllib2, urlparse, base64
 from optparse import OptionParser
 from HTMLParser import HTMLParser
 
-_url = 'https://edrn.nci.nih.gov/snapshots'
+_baseURL = 'https://edrn.nci.nih.gov/'
 _logFormat = '%(asctime)s %(levelname)-8s %(message)s'
-_realm = 'Database Snapshots'
 _bufsiz = 1024
 
-_op = OptionParser(usage='%prog [options] [URL]', version='0.0.0', description='Gets the latest EDRN DB snapshot')
+_op = OptionParser(usage='%prog [options] [BASEURL]', version='0.0.0', description='Gets the latest EDRN DB snapshot')
 _op.add_option('-u', '--user', dest='user', metavar='USER', default='snapshots', help='Log in with USER (default "snapshots")')
 _op.add_option('-p', '--password', dest='password', metavar='PASSWORD', help='Use PASSWORD to authenticate')
-_op.add_option('-d', '--dir', dest='directory', metavar='DIR', default='snapshot', help='Write to DIR (default "snapshot")')
+_op.add_option('-d', '--dir', dest='directory', metavar='DIR', default='var', help='Write to DIR (default "var")')
 _op.add_option('-q', '--quiet', action='store_true', default=False, dest='quiet', help='Be quiet, defaults to chatty')
 
 class FileFinder(HTMLParser):
-    def __init__(self):
+    '''Parser for an Apache index page, gathering all links to files and subdirectories.'''
+    def __init__(self, url):
         HTMLParser.__init__(self) # please use newstyle classes ffs
-        self.fileLinks = []
+        self.fileLinks, self.dirLinks = [], []
+        self.basePath = self._getParent(urlparse.urlparse(url).path)
+        if not self.basePath.endswith('/'):
+            self.basePath += '/'
+    def _getParent(self, path):
+        '''Get the parent of a url path that identifies a directory:
+        
+        * /x/y/z/ → /x/y/
+        * /x/y/ → /x/
+        * /x/ → /
+        * / → /'''
+        parent = '/'.join(path.split('/')[:-2])
+        return parent if parent else '/'
     def handle_starttag(self, tag, attrs):
+        '''Just look at anchor elements with href attributes.'''
         if tag == 'a':
             attrs = dict(attrs)
             link = attrs.get('href', None)
-            if link and link != '/':
-                self.fileLinks.append(link)
+            if link:
+                if link.endswith('/'):
+                    if link != self.basePath:
+                        self.dirLinks.append(link[:-1])
+                else:
+                    self.fileLinks.append(link)
                 
 
-def buildOpener(url, user, password):
-    auth = urllib2.HTTPBasicAuthHandler()
-    auth.add_password(_realm, url, user, password)
-    return urllib2.build_opener(auth)
+def openConnection(url, user, password):
+    request = urllib2.Request(url)
+    authString = base64.encodestring('%s:%s' % (user, password)).replace('\n', '')
+    request.add_header('Authorization', 'Basic %s' % authString)
+    return urllib2.urlopen(request)
 
 
 def grab(url, user, password, target):
     con = None
     try:
         logging.info('Getting index of "%s"', url)
-        opener = buildOpener(url, user, password)
-        con = opener.open(url)
-        fileFinder = FileFinder()
+        con = openConnection(url, user, password)
+        fileFinder = FileFinder(url)
         fileFinder.feed(con.read())
         con.close()
-        logging.info('Found %d files to download as part of the snapshot', len(fileFinder.fileLinks))
+        logging.info('Files: %d; directories: %d', len(fileFinder.fileLinks), len(fileFinder.dirLinks))
         for fn in fileFinder.fileLinks:
             src = dst = None
             try:
-                srcURL, targetFile = url + '/' + fn, os.path.join(target, fn)
-                logging.info('Copying "%s" to "%s"', srcURL, targetFile)
-                opener = buildOpener(srcURL, user, password)
-                src, dst = opener.open(srcURL), open(targetFile, 'wb')
+                srcURL, targetFile = url + fn, os.path.join(target, fn)
+                src, dst = openConnection(srcURL, user, password), open(targetFile, 'wb')
+                logging.info('Copying "%s" to "%s" of size %s', srcURL, targetFile, src.info().get('Content-length', 'UNKNOWN'))
                 while True:
                     buf = src.read(_bufsiz)
                     if buf == '': break
@@ -65,6 +81,11 @@ def grab(url, user, password, target):
                         stream.close()
                     except:
                         pass
+        for dn in fileFinder.dirLinks:
+            sub = os.path.join(target, dn)
+            if not os.path.exists(sub):
+                os.makedirs(sub)
+            grab(url + dn + '/', user, password, sub)
         logging.info('Done.')
     finally:
         try:
@@ -76,15 +97,18 @@ def grab(url, user, password, target):
 def main(argv):
     options, args = _op.parse_args(argv)
     if len(args) > 2:
-        _op.error('Specify an (optional) URL for the database snapshot.')
+        _op.error('Specify an (optional) base URL for the database snapshot.')
     elif len(args) == 2:
-        url = args[1]
+        baseURL = args[1]
     else:
-        url = _url
+        baseURL = _baseURL
     if options.quiet:
         logging.basicConfig(level=logging.WARNING, format=_logFormat)
     else:
         logging.basicConfig(level=logging.INFO, format=_logFormat)
+    if not baseURL.endswith('/'):
+        logging.notice('Adding a "/" to base URL "%s"' % baseURL)
+        baseURL += '/'
     if options.password:
         password = options.password
     else:
@@ -93,12 +117,12 @@ def main(argv):
     if os.path.exists(target) and not os.path.isdir(target):
         logging.critical('"%s" already exists and is not a directory; not overwriting it', target)
         sys.exit(1)
-    if not os.path.exists(target):
-        os.makedirs(target)
-    if len(os.listdir(target)) > 0:
-        logging.critical('"%s" already has some files in it; cowardly refusing to use it', target)
-        sys.exit(1)
-    grab(url, user, password, target)
+    for dirname in ('snapshots', 'blobstorage'):
+        d = os.path.join(target, dirname)
+        if not os.path.exists(d):
+            os.makedirs(d)
+        grab(baseURL + dirname + '/', user, password, d)
+
 
 if __name__ == '__main__':
     main(sys.argv)
